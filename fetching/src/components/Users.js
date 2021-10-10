9d0c523f2e85a6e301525488c1185321fe0f5c0d
Marhamat <75548742+Marhamat92@users.noreply.github.com>
2021-10-11 01:49:56 +0300

    fetching added with axios and fetch

---
+++ fetching/src/components/Users.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import {useState,useEffect} from "react"
+import Axios from "axios";
+
+function Users() {
+
+    const [users,setUsers] = useState([])
+    const [isLoading,setIsloading] = useState(true)
+
+
+//useEffect mount yapmak ıcın kullandık ve mount yaparken fetch yap dedik
+useEffect(()=>{
+
+
+//Axios yontemi kullanarak    
+Axios("https://jsonplaceholder.typicode.com/users")
+.then((res)=>setUsers(res.data))
+.catch((e)=>console.log(e))  //yalnısları olursa
+.finally(()=>setIsloading(false))  // false yaptık yukleme olduktan sonra loading gitsin
+
+},[])
+
+
+//fetch yontemi kullanarak
+// fetch("https://jsonplaceholder.typicode.com/users")
+// .then((res)=>res.json())
+// .then((data)=>setUsers(data))
+// .catch((e)=>console.log(e))  //yalnısları olursa
+// .finally(()=>setIsloading(false))  // false yaptık yukleme olduktan sonra loading gitsin
+
+// },[])
+
+
+
+
+
+return (
+        <div>
+            <h1>Users</h1>
+      {isLoading && <div>Loading...</div>}      
+{users.map((user)=>(
+    <div key={user.id} > {user.name} </div>
+))
+}
+
+
+
+        </div>
+    )
+}
+
+export default Users
